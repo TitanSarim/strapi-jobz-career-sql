1a17ebd04cce762f8e3cd00af36f36660c2692e8
Sarim Xahid <muhammadsarim203@gmail.com>
2022-10-07 17:33:47 +0500

    first commit

--- config/database.js
+++ config/database.js
@@ -2,12 +2,28 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'jobzcareer'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'root'),
+      database: env('DATABASE_NAME', 'bbegfpss_jobzcareer'),
+      user: env('DATABASE_USERNAME', 'bbegfpss_root'),
+      password: env('DATABASE_PASSWORD', 'Dani@444'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
 });
+
+
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'mysql',
+//     connection: {
+//       host: env('DATABASE_HOST', '127.0.0.1'),
+//       port: env.int('DATABASE_PORT', 3306),
+//       database: env('DATABASE_NAME', 'jobzcareer'),
+//       user: env('DATABASE_USERNAME', 'root'),
+//       password: env('DATABASE_PASSWORD', 'root'),
+//       ssl: env.bool('DATABASE_SSL', false),
+//     },
+//   },
+// });
